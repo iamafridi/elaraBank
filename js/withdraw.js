72488b7ca161b8e03155e87bdeef188fa34df344
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-08-29 17:02:09 +0600

    calculations done

--- js/withdraw.js
+++ js/withdraw.js
@@ -25,30 +25,3 @@ balanceTotalElement.innerText = newBalanceTotal;
 
 
 });
-
-
-// document.getElementById("btn-deposit").addEventListener("click", function () {
-//     const depositField = document.getElementById("deposit-field");
-//     const newDepositAmountString = depositField.value;
-//     // console.log(newDepositAmountString)
-//     const newDepositAmount = parseFloat(newDepositAmountString);
-  
-//     depositField.value = '';
-//     const depositTotalElement = document.getElementById("deposit-total");
-//     const previousDepositTotalString = depositTotalElement.innerText;
-//     const previousDepositTotal = parseFloat(previousDepositTotalString);
-  
-//     const newDepositTotal = previousDepositTotal + newDepositAmount;
-//     depositTotalElement.innerText = newDepositTotal;
-  
-//     const balanceTotalElement = document.getElementById('balance-total');
-//   const previousBalanceTotalString = balanceTotalElement.innerText;
-//   const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
-  
-//   const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
-//   balanceTotalElement.innerText = newBalanceTotal;
-  
-  
-  
-//   });
-  
